30d78506d6b1e7570073f05b3f9575ba13f72ded
AxelAFV <axel.fernandezvalverde@gmail.com>
2025-12-30 20:50:16 +0000

    nuevo

--- src/js/components/DisplayCount.jsx
+++ src/js/components/DisplayCount.jsx
@@ -1,18 +1,14 @@
-import { Contador } from "./Contador";
-import { Count5, Count4, Count3, Count2, Count1 } from "../main";
-export const DisplayCount = ({ Counter }) => {
-  let ObjetoEspecificoIcono = {
-    icono: <i className="fa-solid fa-clock"></i>,
-  };
+export const DisplayCount = ({ unidad, decena, centena, unidadM, decenaM, centenaM }) => {
+  
   return (
-    <div className="row bg-dark bg-opacity justify-content-evenly my-auto text-white">
-      <Contador id={0} digit={ObjetoEspecificoIcono.icono} />
-      <Contador id={1} digit={Count1} />
-      <Contador id={2} digit={Count2} />
-      <Contador id={3} digit={Count3} />
-      <Contador id={4} digit={Count4} />
-      <Contador id={5} digit={Count5} />
-      <Contador id={6} digit={Counter} />
+    <div className="justify-content-evenly bg-dark bg-opacity py-5 text-center d-flex fs-1 text-white">
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5"><i className="fa-solid fa-clock "></i></div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{centenaM % 10}</div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{decenaM % 10}</div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{unidadM % 10}</div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{centena % 10}</div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{decena % 10}</div>
+    <div className="col-1 rounded bg-secondary mx-3 px-2 py-5">{unidad % 10}</div>
     </div>
   );
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -13,29 +13,18 @@ import { DisplayCount } from "./components/DisplayCount";
 import { OpcionCuentaRegresiva } from "./OpcionCuentaRegresiva";
 
 const ReactRender = ReactDOM.createRoot(document.getElementById("root"));
-let Count = 0;
-export let Count5 = 0;
-export let Count4 = 0;
-export let Count3 = 0;
-export let Count2 = 0;
-export let Count1 = 0;
-// No consegui manera logica de hacer que los contadores Count no se actualizaran a cero dentro del componente DisplayCount.jsx, y queria usar los hooks, pero no los comprendo muy bien aun. Por eso los declare en el Main
-// Supongo que es malaPraxis im sorry
+let count = 0;
 setInterval(() => {
-  Count > 9 ? (Count = 0) : true;
-  Count === 9 ? Count5++ : true;
-  Count5 === 9 ? Count4++ : true;
-  Count5 === 9 ? (Count5 = 0) : true;
-  Count4 === 9 ? Count3++ : true;
-  Count4 === 9 ? (Count4 = 0) : true;
-  Count3 === 9 ? Count2++ : true;
-  Count3 === 9 ? (Count3 = 0) : true;
-  Count2 === 9 ? Count1++ : true;
-  Count2 === 9 ? (Count2 = 0) : true;
-  ReactRender.render(
+  const centenaM = Math.floor(count / 100000);
+  const decenaM = Math.floor(count / 10000);
+  const unidadM = Math.floor(count / 1000);
+  const centena = Math.floor(count / 100);
+  const decena = Math.floor(count / 10);
+  const unidad = Math.floor(count / 1);
+    ReactRender.render(
     <React.StrictMode>
-      <DisplayCount Counter={Count} />
+      <DisplayCount unidad={unidad} decena={decena} centena={centena} unidadM={unidadM} decenaM={decenaM} centenaM={centenaM}/>
     </React.StrictMode>
-  );
-  Count++;
+  ); 
+  count ++
 }, 1);
